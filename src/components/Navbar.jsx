3239b64f47a6dfd3c38f0341ac74c9087629fbc6
zarir-engineer <zarir.utube@gmail.com>
2026-03-12 15:54:56 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,17 @@ const Navbar = ({ is3D, onToggle, onAddClick }) => {
   return (
     <div className="navbar-container">
       
-      {/* 1. LEFT SIDE: The 2D/3D Switch */}
-      <div className="switch-wrapper">
-        <ToggleSwitch is3D={is3D} onToggle={onToggle} />
+      {/* 1. LEFT SIDE: The 2D/3D Switch + Back Button */}
+      <div className="left-group">
+        <a className="back-pill" href="https://nishith.is-a.dev/projects/" title="Back to Portfolio">
+          <svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
+            <path d="M19 12H5M12 5l-7 7 7 7"/>
+          </svg>
+          Portfolio
+        </a>
+        <div className="switch-wrapper">
+          <ToggleSwitch is3D={is3D} onToggle={onToggle} />
+        </div>
       </div>
 
       {/* 2. RIGHT SIDE: The Add Button */}
